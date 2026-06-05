b5b39b75d8b3feb4fccd0f8ff823af474dec5af6
Ayaan-Mahajan <mahajanayaan60@gmail.com>
2026-06-05 13:01:49 +0530

    Initial Version of Gym Tracker

---
+++ README.md
@@ -0,0 +1,23 @@
+# Gym Tracker
+A simple Python-based Gym Tracker
+
+## Features
+- Add Workouts
+- View Workout History
+- Save Workouts using JSON
+
+## Technologies Used
+- Python
+- JSON
+- Git & GitHub
+
+## Future Improvements
+- Add Date Tracking
+- Add Muscle Groups
+- Add Workout Statistics
+- Build A GUI/Web version
+
+# Author
+Ayaan Mahajan
+
+Built as my first programming project
---
+++ Tracker.py
@@ -0,0 +1,39 @@
+import json
+try:
+    with open("workouts.json", "r") as file:
+        workouts=json.load(file)
+except:
+    workouts=[]
+while True:
+    print("\n-----Gym Tracker-----")
+    print("1. Add Workout")
+    print("2. View Workouts")
+    print("3. Exit")
+    choice=int(input('Enter choice: '))
+    
+    if choice==1:
+        ex=input('Enter Exercise: ')
+        weight=(input('Enter Weight(kg): '))
+        rep=int(input('Enter Reps: '))
+        workout={"Exercise": ex, "Weight": weight, "Reps": rep}
+        workouts.append(workout)
+        with open("workouts.json", "w") as file:
+            json.dump(workouts, file, indent=4)
+        print('Workout Added Successfully!')
+        
+    elif choice==2:
+        print('\nWorkout History: ')
+        if workouts==[]:
+            print('No Workouts Found. ')
+        else:
+            for workout in workouts:
+                print(f"{workout["Exercise"]} - "
+                          f"{workout["Weight"]} x "
+                          f"{workout["Reps"]} reps")
+        
+    elif choice==3:
+        print('Goodbye!')
+        break
+    else:
+        print('Invalid Choice')
+    
